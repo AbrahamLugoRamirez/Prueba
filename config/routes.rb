ed0b3a20a558772f575eb4dbfe67f6dbb569f9a6
njmercado <njmercado@uninorte.edu.co>
2020-09-20 17:26:42 -0500

    Ruby 2.7.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'signup', to: 'signup#index'
   get '/login', to: 'login#index'
   get '/', to: 'login#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
