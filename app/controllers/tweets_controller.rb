539c6af8775464e53f439719cc8bce8414d68452
AbrahamLugoRamirez <labrahamdedios@gmail.com>
2020-09-24 12:45:11 -0500

    Add tweet controler, index, create, show

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,23 @@
+class TweetsController < ApplicationController
+
+def index
+    @tweets = Tweet.all
+end
+
+def show
+    @tweets = Tweet.all
+end
+
+def create
+    @tweets = Tweet.new(tweets_params)
+    if @tweets.save
+        redirect_to tweets_path
+    end
+end
+
+private
+def tweets_params
+    params.require(:tweet).permit(:username, :description)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'login#index'
+  get '/s' => 'login#index'
   get 'dashboard' => 'dashboard#index'
+  
+  resource :tweets
 end
