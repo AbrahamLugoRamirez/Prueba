50fd90c4a8eebb5b93ee7c479d9e4d850f631724
njmercado <njmercado@uninorte.edu.co>
2020-09-30 12:32:45 -0500

    Agregados campos de nombre e imagen para el usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_151535) do
+ActiveRecord::Schema.define(version: 2020_09_30_172856) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "username"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2020_09_24_151535) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
