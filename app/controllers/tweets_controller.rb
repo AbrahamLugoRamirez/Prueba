07e8a39fe66d778d1990a249c2db8ac7df851270
AbrahamLugoRamirez <labrahamdedios@gmail.com>
2020-09-30 12:35:11 -0500

    Delete tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,12 +7,26 @@ end
 def show
     @tweets = Tweet.all
 end
+def new
+    @tweets = Tweet.show
+  end
 
 def create
     @tweets = Tweet.new(tweets_params)
-    if @tweets.save
-        redirect_to tweets_path
-    end
+    
+        if @tweets.save
+            redirect_to tweets_path, notice: 'hello'
+            
+        else
+            redirect_to tweets_path
+
+        end
+    
+end
+def destroy
+    @post = Tweet.find(params[:id])
+    @post.destroy
+    redirect_to tweets_path
 end
 
 private
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ApplicationRecord
+     validates_length_of :description, :maximum => 1, :presence => { message: "No puede dejarse vacío" }
+    
+
 end
